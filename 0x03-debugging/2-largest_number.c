08f41cff71f820274734e1cab53f4b8ee53674ea
mariyemaGuen <guenziz.mariyem@gmail.com>
2023-06-21 10:24:31 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -29,6 +29,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	
+
 return (largest);
 }
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - prints if integer is positive or negative
+ * @i: integer to check
+ */
+void positive_or_negative(int i)
+{
+        if (i > 0)
+                printf("%d is positive\n", i);
+        else if (i < 0)
+                printf("%d is negative\n", i);
+        else
+                printf("%d is zero\n", i);
+}
