adb58e68fb3c537c608a6273fdbf22a191dc9a83
mariyemaGuen <guenziz.mariyem@gmail.com>
2023-07-07 18:49:28 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,29 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-/**
- * _strcat - concatenates two strings
- * @dest: destination string
- * @src: source string
- *
- * Return: pointer to the resulting string
- */
 char *_strcat(char *dest, char *src)
 {
-int i = 0;
-int j = 0;
-
-while (dest[i] != '\0')
-{
-	i++;
-}
-
-while (src[j] != '\0')
-{
-	dest[i] = src[j];
-	i++;
-	j++;
-	}
-
-dest[i] = '\0';
-return (dest);
+    return strcat(dest, src);
 }
--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,23 +1,10 @@
+#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-void _puts(char *str);
-
-/**
- * _puts - Prints a string to the standard output.
- * @str: The string to be printed.
- */
-void _puts(char *str)
+void _puts(char *s)
 {
-    /* Function implementation goes here */
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-_puts("\"Programming is like building a multilingual puzzle\"");
-return (0);
+    puts(s);
 }
